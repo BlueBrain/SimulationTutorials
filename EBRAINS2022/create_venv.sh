8cff3d62842c031008113a1c904fac75973118c5
Tanguy Pierre Louis Damart <tanguy.damart@epfl.ch>
2022-11-03 16:34:14 +0100

    Add tutorial for EBRAINS2022

---
+++ EBRAINS2022/create_venv.sh
@@ -0,0 +1,14 @@
+deactivate
+
+export VENV=myvenv
+python3 -m venv $VENV
+source $VENV/bin/activate
+
+./$VENV/bin/pip install --upgrade setuptools pip
+./$VENV/bin/pip install wheel
+./$VENV/bin/pip install jupyter
+./$VENV/bin/pip install neurom
+./$VENV/bin/pip install bluepyopt
+./$VENV/bin/pip install git+https://github.com/BlueBrain/BluePyEfe@BPE2
+
+./$VENV/bin/ipython kernel install --name "EBRAINS2022" --user
