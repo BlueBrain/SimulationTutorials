c907c756662253a149d8346739680f60a47b7e10
Tanguy Pierre Louis Damart <tanguy.damart@epfl.ch>
2022-11-07 11:41:19 +0100

    Update setup of BluePyEfe to master branch

--- EBRAINS2022/create_venv.sh
+++ EBRAINS2022/create_venv.sh
@@ -9,6 +9,6 @@ source $VENV/bin/activate
 ./$VENV/bin/pip install jupyter
 ./$VENV/bin/pip install neurom
 ./$VENV/bin/pip install bluepyopt
-./$VENV/bin/pip install git+https://github.com/BlueBrain/BluePyEfe@BPE2
+./$VENV/bin/pip install git+https://github.com/BlueBrain/BluePyEfe
 
 ./$VENV/bin/ipython kernel install --name "EBRAINS2022" --user
